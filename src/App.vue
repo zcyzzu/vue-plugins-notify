da621b02b01874d1352073f8292658432f1cbe7c
zcyzzu <zcy2764707016@gmail.com>
2021-04-29 11:05:53 +0800

    delete useless code

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <button @click="zcy">123</button>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-  methods: {
-    zcy() {
-      this.notify({
-        content: "65456",
-      });
-    },
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
